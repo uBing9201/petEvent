1c5d8bc2bbf813bf3c0fa6c1b71293f0fa3e66c1
ubing <ubing0101@kakao.com>
2025-06-27 12:08:42 +0900

    Initial commit

---
+++ src/main/java/com/playdata/petevent/PetEventApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.petevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playdata/petevent/PetEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.petevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
